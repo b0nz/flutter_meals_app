747d29869df5d4076eaf255480883fcc785ce557
b0nz <elliantosarono.p@gmail.com>
2022-11-24 20:08:11 +0700

    add filters screen

---
+++ lib/screens/filters_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
+
+class FiltersScreen extends StatelessWidget {
+  static const routeName = '/filters';
+
+  const FiltersScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
+      drawer: const MainDrawer(),
+      body: const Center(
+        child: Text('Filters!'),
+      ),
+    );
+  }
+}
